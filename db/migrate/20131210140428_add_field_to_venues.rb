82627434e5ce0e838233b83d92d16063340f9d75
On Ha Chung Phuoc <onhachungphuoc@gmail.com>
2013-12-12 19:35:28 +0700

    add view image

--- db/migrate/20131210140428_add_field_to_venues.rb
+++ db/migrate/20131210140428_add_field_to_venues.rb
@@ -2,7 +2,7 @@ class AddFieldToVenues < ActiveRecord::Migration
   def self.up
     add_column :venues, :lan, :string
     add_column :venues, :lat, :string
-    add_column :venues, :image, :string
+    add_column :venues, :image, :text
     add_column :venues, :date__venue, :date
   end
 
